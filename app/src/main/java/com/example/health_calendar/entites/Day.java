7fa25aba41e6dd2c267567e2dd7620c1a41db4b5
okwhy <59335640+okwhy@users.noreply.github.com>
2023-11-02 23:54:19 +0400

    day entity and dao creation

---
+++ app/src/main/java/com/example/health_calendar/entites/Day.java
@@ -0,0 +1,20 @@
+package com.example.health_calendar.entites;
+
+import androidx.room.Entity;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Setter
+@Getter
+@Entity
+public class Day implements Serializable {
+}
