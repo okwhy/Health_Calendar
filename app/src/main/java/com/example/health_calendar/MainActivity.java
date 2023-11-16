328cac21f627fc790847a5ff51f91c04be861215
121Artem122 <99825505+121Artem122@users.noreply.github.com>
2023-11-16 23:29:12 +0400

    Поля
    Создали доп поля

--- app/src/main/java/com/example/health_calendar/MainActivity.java
+++ app/src/main/java/com/example/health_calendar/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private int daysIndex = 0;
     private int monthsIndex = 0;
     private int yearIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,37 +34,88 @@ public class MainActivity extends AppCompatActivity {
         CalendarView calendarView = findViewById(R.id.calendarView);
 
         final List<String> calendarStrings = new ArrayList<>();
-        final int[] days= new int[30];
+        final int[] days = new int[31];
         final int[] months = new int[12];
         final int[] years = new int[10];
 
-        final EditText textInput = findViewById(R.id.textInput);
+        final TextView textHeight = findViewById(R.id.textHeight);
+        final EditText textInputHeight = findViewById(R.id.textInputHeight);
 
-        final View dayContent = findViewById(R.id.dayContent);
+        final TextView textWeight = findViewById(R.id.textWeight);
+        final EditText textInputWeight = findViewById(R.id.textInputWeight);
+
+        final TextView textPulse = findViewById(R.id.textPulse);
+        final EditText textInputPulse = findViewById(R.id.textInputPulse);
+
+        final TextView textPressure = findViewById(R.id.textPressure);
+        final EditText textInputPressure = findViewById(R.id.textInputPressure);
+
+        final TextView textAppetite = findViewById(R.id.textAppetite);
+        final EditText textInputAppetite = findViewById(R.id.textInputAppetite);
+
+        final TextView textSlepping = findViewById(R.id.textSlepping);
+        final EditText textInputSlepping = findViewById(R.id.textInputSlepping);
+
+        final TextView textExercise = findViewById(R.id.textExercise);
+        final EditText textInputExercise = findViewById(R.id.textInputExercise);
+
+        final TextView textHealth = findViewById(R.id.textHealth);
+        final EditText textInputHealth = findViewById(R.id.textInputHealth);
+
+        final View dayInfo = findViewById(R.id.dayInfo);
+        final View dayHeight = findViewById(R.id.dayHeight);
+        final View dayWeight = findViewById(R.id.dayWeight);
+        final View dayPulse = findViewById(R.id.dayPulse);
+        final View dayPressure = findViewById(R.id.dayPressure);
+        final View dayAppetite = findViewById(R.id.dayAppetite);
+        final View daySlepping = findViewById(R.id.daySlepping);
+        final View dayExercise = findViewById(R.id.dayExercise);
+        final View dayHealth = findViewById(R.id.dayHealth);
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 currentYear = year;
                 currentMonth = month;
                 currentDay = dayOfMonth;
-                if(dayContent.getVisibility() == View.GONE)
-                {
-                    dayContent.setVisibility(View.VISIBLE);
+
+                if (dayInfo.getVisibility() == View.GONE &&
+                        dayHeight.getVisibility() == View.GONE &&
+                        dayWeight.getVisibility() == View.GONE &&
+                        dayPulse.getVisibility() == View.GONE &&
+                        dayPressure.getVisibility() == View.GONE &&
+                        dayAppetite.getVisibility() == View.GONE &&
+                        daySlepping.getVisibility() == View.GONE &&
+                        dayExercise.getVisibility() == View.GONE &&
+                        dayHealth.getVisibility() == View.GONE
+                ) {
+                    dayInfo.setVisibility(View.VISIBLE);
+                    dayHeight.setVisibility(View.VISIBLE);
+                    dayWeight.setVisibility(View.VISIBLE);
+                    dayPulse.setVisibility(View.VISIBLE);
+                    dayPressure.setVisibility(View.VISIBLE);
+                    dayAppetite.setVisibility(View.VISIBLE);
+                    daySlepping.setVisibility(View.VISIBLE);
+                    dayExercise.setVisibility(View.VISIBLE);
+                    dayHealth.setVisibility(View.VISIBLE);
                 }
 
-                for(int i = 0; i < 30; i++)
-                {
-                    if(days[i] == currentDay)
-                    {
-                        for(int j = 0; j < 12; j++)
-                        {
-                            if(months[j] == currentMonth)
-                            {
-                                for(int k = 0; k < 10; k++)
-                                {
-                                    if(years[k] == currentYear)
-                                    {
-                                        textInput.setText(calendarStrings.get(i));
+                for (int i = 0; i < 30; i++) {
+                    if (days[i] == currentDay) {
+                        for (int j = 0; j < 12; j++) {
+                            if (months[j] == currentMonth) {
+                                for (int k = 0; k < 10; k++) {
+                                    if (years[k] == currentYear) {
+                                        calendarView.set
+                                        calendarView.setBackgroundColor(10);
+                                        textInputHeight.setText(calendarStrings.get(i));
+                                        textInputWeight.setText(calendarStrings.get(i + 1));
+                                        textInputPulse.setText(calendarStrings.get(i + 2));
+                                        textInputPressure.setText(calendarStrings.get(i + 3));
+                                        textInputAppetite.setText(calendarStrings.get(i + 4));
+                                        textInputSlepping.setText(calendarStrings.get(i + 5));
+                                        textInputExercise.setText(calendarStrings.get(i + 6));
+                                        textInputHealth.setText(calendarStrings.get(i + 7));
                                         return;
                                     }
                                 }
@@ -71,13 +123,19 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 }
-                textInput.setText("");
+                textInputHeight.setText("");
+                textInputWeight.setText("");
+                textInputPulse.setText("");
+                textInputPressure.setText("");
+                textInputAppetite.setText("");
+                textInputSlepping.setText("");
+                textInputExercise.setText("");
+                textInputHealth.setText("");
             }
         });
 
 
         final Button saveTextButton = findViewById(R.id.saveTextButton);
-
 
 
         saveTextButton.setOnClickListener(new View.OnClickListener() {
@@ -86,12 +144,25 @@ public class MainActivity extends AppCompatActivity {
                 days[daysIndex] = currentDay;
                 months[monthsIndex] = currentMonth;
                 years[yearIndex] = currentYear;
-                calendarStrings.add(daysIndex, textInput.getText().toString());
+                calendarStrings.add(daysIndex, textInputHealth.getText().toString());
+                calendarStrings.add(daysIndex, textInputExercise.getText().toString());
+                calendarStrings.add(daysIndex, textInputSlepping.getText().toString());
+                calendarStrings.add(daysIndex, textInputAppetite.getText().toString());
+                calendarStrings.add(daysIndex, textInputPressure.getText().toString());
+                calendarStrings.add(daysIndex, textInputPulse.getText().toString());
+                calendarStrings.add(daysIndex, textInputWeight.getText().toString());
+                calendarStrings.add(daysIndex, textInputHeight.getText().toString());
                 daysIndex++;
                 monthsIndex++;
                 yearIndex++;
-                textInput.setText("");
-
+                textInputHeight.setText("");
+                textInputWeight.setText("");
+                textInputPulse.setText("");
+                textInputPressure.setText("");
+                textInputAppetite.setText("");
+                textInputSlepping.setText("");
+                textInputExercise.setText("");
+                textInputHealth.setText("");
             }
         });
     }
