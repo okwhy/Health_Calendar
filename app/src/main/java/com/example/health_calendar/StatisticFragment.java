073669233cbcad85b3b3c2e921b9ec79fd85dab9
121Artem122 <99825505+121Artem122@users.noreply.github.com>
2024-01-05 18:43:35 +0400

    изменение окна статистики

--- app/src/main/java/com/example/health_calendar/StatisticFragment.java
+++ app/src/main/java/com/example/health_calendar/StatisticFragment.java
@@ -1,6 +1,6 @@
 package com.example.health_calendar;
 
-import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,20 +8,26 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarEntry;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import androidx.fragment.app.Fragment;
-import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ArrayList;
 
 public class StatisticFragment extends Fragment {
 
@@ -29,29 +35,137 @@ public class StatisticFragment extends Fragment {
         // require a empty public constructor
     }
 
+    private LineDataSet createLineDataSet(List<Entry> entries, String label, int color) {
+        LineDataSet dataSet = new LineDataSet(entries, label);
+        dataSet.setColor(color);
+        dataSet.setCircleColor(color);
+        dataSet.setLineWidth(2f);
+        dataSet.setCircleRadius(4f);
+        dataSet.setValueTextSize(10f);
+        dataSet.setDrawValues(false);
+        return dataSet;
+    }
+
+    private void setupChart(LineChart chart) {
+
+        Description description = new Description();
+        description.setText("Артериальное давление");
+        chart.setDescription(description);
+
+        Legend legend = chart.getLegend();
+        legend.setForm(Legend.LegendForm.LINE);
+        legend.setTextSize(12f);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(12f);
+
+        YAxis leftYAxis = chart.getAxisLeft();
+        leftYAxis.setTextSize(12f);
+
+    }
+
+    private ViewFlipper simpleViewFlipper;
+    Button btnNext;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.statistic_page, container, false);
 
+        // get The references of Button and ViewFlipper
+        btnNext = (Button) rootView.findViewById(R.id.buttonNext);
+        simpleViewFlipper = (ViewFlipper) rootView.findViewById(R.id.simpleViewFlipper); // get the reference of ViewFlipper
+        // Declare in and out animations and load them using AnimationUtils class
+        Animation in = AnimationUtils.loadAnimation(getContext(), android.R.anim.slide_in_left);
+        Animation out = AnimationUtils.loadAnimation(getContext(), android.R.anim.slide_out_right);
 
-        BarChart barChart = rootView.findViewById(R.id.barChart);
+        // set the animation type to ViewFlipper
+        simpleViewFlipper.setInAnimation(in);
+        simpleViewFlipper.setOutAnimation(out);
 
-        // Создадим данные для диаграммы
-        List<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(1, 50f));
-        entries.add(new BarEntry(2, 80f));
-        entries.add(new BarEntry(3, 60f));
-        entries.add(new BarEntry(4, 30f));
-        entries.add(new BarEntry(5, 90f));
+        // ClickListener for NEXT button
+        // When clicked on Button ViewFlipper will switch between views
+        // The current view will go out and next view will come in with specified animation
+        btnNext.setOnClickListener(new View.OnClickListener() {
 
-        BarDataSet dataSet = new BarDataSet(entries, "Пример столбчатой диаграммы");
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                // show the next view of ViewFlipper
+                simpleViewFlipper.showNext();
+            }
+        });
 
-        // Создадим объект данных и передадим набор данных
-        BarData barData = new BarData(dataSet);
+        //РОСТ
+        BarChart barChartHeight = rootView.findViewById(R.id.barChartHeight);
 
-        // Установим данные в BarChart
-        barChart.setData(barData);
+        List<BarEntry> entriesHeight = new ArrayList<>();
+        entriesHeight.add(new BarEntry(1, 176f));
+        entriesHeight.add(new BarEntry(2, 176f));
+        entriesHeight.add(new BarEntry(3, 175f));
+        entriesHeight.add(new BarEntry(4, 177f));
+        entriesHeight.add(new BarEntry(5, 177f));
+
+        BarDataSet dataSetHeight = new BarDataSet(entriesHeight, "Рост");
+        BarData barDataHeight = new BarData(dataSetHeight);
+        barChartHeight.setData(barDataHeight);
+
+        //ВЕС
+        BarChart barChartWeight = rootView.findViewById(R.id.barChartWeight);
+        List<BarEntry> entriesWeight = new ArrayList<>();
+        entriesWeight.add(new BarEntry(1, 64f));
+        entriesWeight.add(new BarEntry(2, 63f));
+        entriesWeight.add(new BarEntry(3, 64f));
+        entriesWeight.add(new BarEntry(4, 65f));
+        entriesWeight.add(new BarEntry(5, 62f));
+
+        BarDataSet dataSetWeight = new BarDataSet(entriesWeight, "Вес");
+        BarData barDataWeight = new BarData(dataSetWeight);
+        barChartWeight.setData(barDataWeight);
+
+        //ЧСС
+        BarChart barChartCHSS = rootView.findViewById(R.id.barChartCHSS);
+        List<BarEntry> entriesCHSS = new ArrayList<>();
+        entriesCHSS.add(new BarEntry(1, 71f));
+        entriesCHSS.add(new BarEntry(2, 78f));
+        entriesCHSS.add(new BarEntry(3, 76f));
+        entriesCHSS.add(new BarEntry(4, 80f));
+        entriesCHSS.add(new BarEntry(5, 75f));
+
+        BarDataSet dataSetCHSS = new BarDataSet(entriesCHSS, "ЧСС");
+        BarData barDataCHSS = new BarData(dataSetCHSS);
+        barChartCHSS.setData(barDataCHSS);
+
+        //АРТЕРИАЛЬНОЕ ДАВЛЕНИЕ
+        LineChart lineChartPressure = rootView.findViewById(R.id.lineChartPressure);
+
+        List<Entry> entriesUpperPressure = new ArrayList<>();
+        List<Entry> entriesLowerPressure = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            entriesUpperPressure.add(new Entry(i, (float) (Math.random() * 20 + 110)));
+            entriesLowerPressure.add(new Entry(i, (float) (Math.random() * 10 + 70)));
+        }
+
+        LineDataSet dataSetUpperPressure = createLineDataSet(entriesUpperPressure, "Верхнее давление", getResources().getColor(R.color.colorUpperPressure));
+        LineDataSet dataSetLowerPressure = createLineDataSet(entriesLowerPressure, "Нижнее давление", getResources().getColor(R.color.colorLowerPressure));
+
+        LineData lineData = new LineData(dataSetUpperPressure, dataSetLowerPressure);
+        setupChart(lineChartPressure);
+        lineChartPressure.setData(lineData);
+
+        //СОН
+        BarChart barChartSleep = rootView.findViewById(R.id.barChartSleep);
+        List<BarEntry> entriesSleep = new ArrayList<>();
+        entriesSleep.add(new BarEntry(1, 8f));
+        entriesSleep.add(new BarEntry(2, 10f));
+        entriesSleep.add(new BarEntry(3, 6f));
+        entriesSleep.add(new BarEntry(4, 8f));
+        entriesSleep.add(new BarEntry(5, 7f));
+
+        BarDataSet dataSetSleep = new BarDataSet(entriesSleep, "Сон");
+        BarData barDataSleep = new BarData(dataSetSleep);
+        barChartSleep.setData(barDataSleep);
 
         return rootView;
     }
